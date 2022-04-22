8bd83b6a2495b41953762a1979b6864232d93cf9
Bruno Amaral Ruotolo <brunoaruotolo@gmail.com>
2022-04-22 16:36:40 -0300

    fix: remove some consoles

--- src/index.js
+++ src/index.js
@@ -8,12 +8,10 @@ const tweets = [];
 app.use(cors());
 app.use(express.json());
 
-
 function signUp() {
   app.post("/sign-up", (req, res) => {
     const body = req.body;
     users.push(body);
-    console.log(users)
     res.send("OK");
   })
 }
@@ -23,7 +21,6 @@ function tweetsPost() {
     const body = req.body;
     const user = users.find((user) => body.username === user.username)
     tweets.push({ ...body, avatar: user.avatar });
-    console.log(tweets)
     res.send("OK");
   })
 }
@@ -32,7 +29,6 @@ function tweetsGet() {
   app.get("/tweets", (req, res) => {
     const lastTweets = tweets.slice(-10);
     res.send(lastTweets);
-    console.log(lastTweets);
   })
 }
 
