f365aef957455fbc4d4737e1ad99d9c3bd97dc08
Bruno Amaral Ruotolo <brunoaruotolo@gmail.com>
2022-04-22 16:35:56 -0300

    feat: twetts get and post

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import cors from "cors"
 
 const app = express();
 const users = [];
+const tweets = [];
 
 app.use(cors());
 app.use(express.json());
 
 
-function SignUp() {
+function signUp() {
   app.post("/sign-up", (req, res) => {
     const body = req.body;
     users.push(body);
@@ -17,6 +18,26 @@ function SignUp() {
   })
 }
 
-SignUp()
+function tweetsPost() {
+  app.post("/tweets", (req, res) => {
+    const body = req.body;
+    const user = users.find((user) => body.username === user.username)
+    tweets.push({ ...body, avatar: user.avatar });
+    console.log(tweets)
+    res.send("OK");
+  })
+}
+
+function tweetsGet() {
+  app.get("/tweets", (req, res) => {
+    const lastTweets = tweets.slice(-10);
+    res.send(lastTweets);
+    console.log(lastTweets);
+  })
+}
+
+signUp();
+tweetsPost();
+tweetsGet();
 
 app.listen(5000, () => console.log("Server On"))
