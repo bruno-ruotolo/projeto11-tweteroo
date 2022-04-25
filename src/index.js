82de4786dd2e41e74b015f67a62b9881f9f535cc
Bruno Amaral Ruotolo <brunoaruotolo@gmail.com>
2022-04-25 12:43:44 -0300

    feat: validation data

--- src/index.js
+++ src/index.js
@@ -11,17 +11,34 @@ app.use(express.json());
 function signUp() {
   app.post("/sign-up", (req, res) => {
     const body = req.body;
-    users.push(body);
-    res.send("OK");
+    const validURL = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    const { username, avatar } = body;
+    res.status(201).send(body)
+
+    if (!(username === "" || avatar === "") && (validURL.test(avatar))) {
+      users.push(body);
+      res.send("OK");
+    } else if (username === "" || avatar === "") {
+      res.status(400).send("Todos os campos são obrigatórios!");
+    } else {
+      res.sendStatus(400);
+    }
   })
 }
 
 function tweetsPost() {
   app.post("/tweets", (req, res) => {
     const body = req.body;
-    const user = users.find((user) => body.username === user.username)
-    tweets.push({ ...body, avatar: user.avatar });
-    res.send("OK");
+    const { username, tweet } = body;
+    const user = users.find((user) => username === user.username);
+    res.status(201).send(body)
+
+    if (!(username === "" || tweet === "")) {
+      tweets.push({ ...body, avatar: user.avatar });
+      res.send("OK");
+    } else {
+      res.status(400).send("Todos os campos são obrigatórios!");
+    }
   })
 }
 
